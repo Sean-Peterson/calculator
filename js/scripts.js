08cd0ff771519929d50c244d3b61247529f6dd83
Sean Peterson <seanpeterson11@gmail.com>
2017-01-09 19:46:50 -0800

    add -, /, * functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var add = function(number1, number2) {
 return number1 + number2;
 }
+var diff = function(number1, number2) {
+return number1 - number2;
+}
+var quotient = function(number1, number2) {
+return number1 / number2;
+}
+var product = function(number1, number2) {
+return number1 * number2;
+}
 
 var number1 = parseInt(prompt("Pick a number:"));
 var number2 = parseInt(prompt("Pick another number:"));
 
-alert(add(number1, number2))
+alert(quotient(number1, number2))
